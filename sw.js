1646ff3020a3d85aa32d4e77eb041eb3f79475ca
hoitian <155430809+hoitian@users.noreply.github.com>
2025-12-24 19:29:06 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE_NAME = 'beampro-v1';
+const assets = ['/', '/index.html', '/index.css', '/manifest.json'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(assets))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((res) => res || fetch(event.request))
+  );
+});
